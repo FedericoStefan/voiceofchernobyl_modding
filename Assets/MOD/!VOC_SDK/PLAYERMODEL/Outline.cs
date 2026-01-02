9560193926500f3cc1e0f3ec1c7cce92233d04d4
FedericoStefan <64701524+FedericoStefan@users.noreply.github.com>
2026-01-02 21:34:18 +0100

    Delete Assets/MOD/!VOC_SDK/PLAYERMODEL directory

--- Assets/MOD/!VOC_SDK/PLAYERMODEL/Outline.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Outline : MonoBehaviour
-{
-    //Adds support for outlines, as seen in gamemodes with teams when someone is labeled friendly
-    
-    //You should add this on the animator (like base playermodels) or somewhere that is the parent of all renders (skinned mesh and individual spawned objects)
-}
